b1d96b66ab0ad57c85e69995a05dcc170dba37e0
Naveen-Singh8783 <naveensingh8783@gmail.com>
2021-06-15 23:06:50 +0530

    Day-5

---
+++ Patterns/christmasTree.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class christmasTree{
+    public static void main(String[] args){
+        int height = 5; //tree has 5 branchs;
+        int star = 1;
+        int space = height*5;
+
+        for(int tree=1;tree<=height;tree++){
+
+            for(int row=1;row<=height;row++){
+
+                for(int col=1;col<=space;col++)
+                    System.out.print(" ");
+
+                for(int col=1;col<=star;col++)
+                    System.out.print(" *");
+
+                if(row==height){
+                    space+=2;
+                    star-=2;
+                }else{
+                    space--;
+                    star++;
+                }
+                System.out.println();
+            }
+        }
+        space = height*5-4;
+        star = height;
+        for(int rows=1;rows<=height;rows++){
+            for(int col=1;col<=space;col++)
+                System.out.print(" ");
+
+            for(int col=1;col<=star;col++)
+                System.out.print(" *");
+
+            System.out.println();
+        }
+
+    }
+}
---
+++ Patterns/rightAngleFlag.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class rightAngleFlag{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the height of tree greater than 3:");
+        int height = sc.nextInt();
+        int star = 1;
+        int space = height*height;
+
+        for(int tree=1;tree<=height;tree++){
+
+            for(int row=1;row<=height;row++){
+
+                for(int col=1;col<=space;col++)
+                    System.out.print(" ");
+
+                for(int col=1;col<=star;col++)
+                    System.out.print("*");
+
+                if(row==height){
+                    space+=2;
+                    star-=2;
+                }else{
+                    space--;
+                    star++;
+                }
+                System.out.println();
+            }
+        }
+    }
+}
